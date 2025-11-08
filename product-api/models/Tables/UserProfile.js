1b465be343de0a999070362981aeeb21279a6216
botaniqofficialstore <botaniqofficialstore@gmail.com>
2025-11-08 16:42:36 +0530

    conflict flies fixed

--- product-api/models/Tables/UserProfile.js
+++ product-api/models/Tables/UserProfile.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-=======
-
-//----------------------------------------------------------------------
+//---------------------------------------------------------------------
 /** Customer Table */
->>>>>>> dfdff9d8cd23a9cdbbd4a1bc6a20349b46a4ff7a
 const UserProfileSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   firstName: { type: String },
   lastName: { type: String },
-<<<<<<< HEAD
   email: { type: String, unique: true, sparse: true },
   mobileNumber: { type: String, unique: true, sparse: true },
   otp: {
@@ -18,14 +13,6 @@ const UserProfileSchema = new mongoose.Schema({
     expiresAt: Date,
   },
   createdAt: { type: Date, default: Date.now },
-=======
-  email: { type: String, required: true, unique: true },
-  mobileNumber: { type: String, unique: true },
-  address: { type: String },
-  password: { type: String }, // hashed password
-  otp: { code: String, expiresAt: Date }, // for OTP verification
-  createdAt: { type: Date, default: Date.now }
->>>>>>> dfdff9d8cd23a9cdbbd4a1bc6a20349b46a4ff7a
 });
 
 module.exports = mongoose.model('UserProfile', UserProfileSchema);
